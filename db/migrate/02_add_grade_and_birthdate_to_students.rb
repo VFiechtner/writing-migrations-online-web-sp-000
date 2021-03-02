95898f2a570f18db1d7336d64f44920afc496b80
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2021-03-02 03:14:53 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,11 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
 
   def change
-    add_column :grade, :integer, :birthdate, :string
+    add_column :students, :grade, :integer 
+  end
+
+  def change 
+    add_column :students, :birthdate, :string 
   end
 
 end
